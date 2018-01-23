a9b0db1e342fb10e852b192275db13d77b6dd038
RetroPete <34187808+RetroPete@users.noreply.github.com>
2018-01-23 23:15:09 +0000

    Changes made in Main Activity

--- app/src/main/java/com/example/android/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeper/MainActivity.java
@@ -9,11 +9,53 @@ public class MainActivity extends AppCompatActivity {
 
     int scoreTeamA = 0;
     int scoreTeamB = 0;
+    int scoreTeamASave;
+    int scoreTeamBSave;
+
+    private String scoreTeam_A = "ScoreTeamA";
+    private String scoreTeam_B = "ScoreTeamB";
+    private String scoreTeamA_Save = "ScoreTeamASave";
+    private String scoreTeamB_Save = "ScoreTeamBSave";
+    private String teamAName = "TeamAName";
+    private String teamBName = "TeamBName";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //onSaveInstanceState
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+
+        super.onSaveInstanceState(outState);
+        outState.putInt(scoreTeam_A, scoreTeamA);
+        outState.putInt(scoreTeam_B, scoreTeamB);
+        outState.putInt(scoreTeamA_Save, scoreTeamASave);
+        outState.putInt(scoreTeamB_Save, scoreTeamBSave);
+        outState.putString("TeamAName", teamAName);
+        outState.putString("TeamBName", teamBName);
+    }
+
+    //onRestoreInstanceState
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+
+        super.onRestoreInstanceState(savedInstanceState);
+
+        // Restore UI state from the savedInstanceState.
+        // This bundle has also been passed to onCreate.
+        scoreTeamA = savedInstanceState.getInt(scoreTeam_A);
+        scoreTeamB = savedInstanceState.getInt (scoreTeam_B);
+        scoreTeamASave = savedInstanceState.getInt(scoreTeamA_Save);
+        scoreTeamBSave = savedInstanceState.getInt (scoreTeamB_Save);
+        teamAName = savedInstanceState.getString("TeamAName");
+        teamBName = savedInstanceState.getString("TeamBName");
+
+
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
     }
 
     /**
